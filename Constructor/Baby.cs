d0bedbbc1bd99afeba8f4f2d017672def3d7d4e2
rabiademirbag <rddemirbagg@gmail.com>
2025-05-17 23:55:18 +0300

    Proje dosyası ekle.

---
+++ Constructor/Baby.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Baby
+    {
+        public Baby() 
+        {
+            Console.WriteLine("Ingaaa");
+            BirthDate = DateTime.Now;
+            _name = "-";
+            _surname = "-";
+        }
+        public Baby(string name, string surname)
+        {
+            Console.WriteLine("Ingaaa");
+            BirthDate = DateTime.Now;
+            _name = name;
+            _surname = surname;
+        }
+
+
+
+        private string _name;
+        private string _surname;
+        private DateTime _birthDate;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+                _surname = value;
+            }
+        }
+        public DateTime BirthDate
+        {
+            get
+            {
+                return _birthDate;
+            }
+            set
+            {
+                _birthDate = value;
+            }
+        }
+        public void DisplayBabyInfo()
+        {
+            Console.WriteLine("Bebek ismi: "+_name+" "+_surname+","+"Doğum tarihi: "+_birthDate);
+        }
+    }
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,13 @@
+﻿using Constructor;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Baby baby = new Baby();
+        baby.DisplayBabyInfo();
+        Baby baby2 = new Baby("Ayşe", "Gürel");
+        baby2.DisplayBabyInfo();
+
+    }
+}
